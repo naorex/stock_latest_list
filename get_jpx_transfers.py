f15b071ce817825fade5b882a3d5fcba1f0d7b70
naorex <nao23r@gmail.com>
2025-10-05 16:02:14 +0900

    UPDATE : get_jpx_transfers.py -> Adopted to html form change

--- get_jpx_transfers.py
+++ get_jpx_transfers.py
@@ -1,32 +1,37 @@
 import pandas as pd
+
 from get_data import get_data
+
 
 def get_jpx_transfers():
 
     # 上場廃止銘柄のデータ元リンク
-    jpx_transfers_url = 'https://www.jpx.co.jp/listing/stocks/transfers/index.html'
+    jpx_transfers_url = "https://www.jpx.co.jp/listing/stocks/transfers/index.html"
 
     # データを取得
     soup = get_data(jpx_transfers_url)
 
     # テーブル部分のみ抽出
-    items = soup.findAll("div", {"class": "component-normal-table"})
+    items = soup.find_all("div", {"class": "component-normal-table"})
 
     # Dataframeに格納する変更日、コード、市場区分を取得
     code_data = []
     for item in items:
-        code_list = item.findAll("td", {"class": "a-center"})
+        code_list = item.find_all("td", {"class": "a-center"})
         for code in code_list:
             code = code.getText().strip()
             code_data.append(code)
 
-    # Dataframeに格納する銘柄名を取得
+    # Dataframeに格納する銘柄名を取得（各行の最初の a-left のみ）
     name_data = []
     for item in items:
-        name_list = item.findAll("td", {"class": "a-left"})
-        for name in name_list:
-            name = name.getText().strip().replace('　',' ')
-            name_data.append(name)
+        # 行単位で処理して最初の a-left を拾う
+        rows = item.find_all("tr")
+        for row in rows:
+            td = row.find("td", {"class": "a-left"})
+            if td:
+                name = td.getText().strip().replace("　", " ")
+                name_data.append(name)
 
     # Dataframe格納用に整理（日付）
     i_pre = []
@@ -43,7 +48,7 @@ def get_jpx_transfers():
         i = i[:4]
         i_pre.append(i)
     i_pre2 = [x for x in i_pre if len(x) < 5 and len(x) > 0]
-    i_pre3 = [x for x in i_pre2 if '-' not in x]
+    i_pre3 = [x for x in i_pre2 if "-" not in x]
 
     for i in i_pre3[1::4]:
         # code_pre.append(int(i))
@@ -56,7 +61,7 @@ def get_jpx_transfers():
         i = i[:6]
         i_pre.append(i)
     i_pre2 = [x for x in i_pre if len(x) < 7 and len(x) > 0]
-    i_pre3 = [x for x in i_pre2 if '-' not in x]
+    i_pre3 = [x for x in i_pre2 if "-" not in x]
 
     for i in i_pre3[2::4]:
         mrkt_pre.append(i)
@@ -68,7 +73,7 @@ def get_jpx_transfers():
         i = i[:6]
         i_pre.append(i)
     i_pre2 = [x for x in i_pre if len(x) < 7 and len(x) > 0]
-    i_pre3 = [x for x in i_pre2 if '-' not in x]
+    i_pre3 = [x for x in i_pre2 if "-" not in x]
 
     for i in i_pre3[3::4]:
         mrkt_pre2.append(i)
@@ -77,21 +82,22 @@ def get_jpx_transfers():
     i_pre = []
     name_pre = []
     for i in name_data:
-        i_pre.append(i.replace('（株）',''))
+        i_pre.append(i.replace("（株）", ""))
 
     for i in i_pre:  # 20250302 外した -> i_pre[::2]
         name_pre.append(i)
 
     # 各データをDataframeへ格納していく
     transfers_df = pd.DataFrame()
-    transfers_df.loc[:,'変更日'] = date_pre
-    transfers_df.loc[:,'銘柄名'] = name_pre
-    transfers_df.loc[:,'コード'] = code_pre
-    transfers_df.loc[:,'市場区分'] = mrkt_pre
-    transfers_df.loc[:,'市場区分(変更前)'] = mrkt_pre2
+    transfers_df.loc[:, "変更日"] = date_pre
+    transfers_df.loc[:, "銘柄名"] = name_pre
+    transfers_df.loc[:, "コード"] = code_pre
+    transfers_df.loc[:, "市場区分"] = mrkt_pre
+    transfers_df.loc[:, "市場区分(変更前)"] = mrkt_pre2
 
     return transfers_df
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     result_df = get_jpx_transfers()
     print(result_df)
